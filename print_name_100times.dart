56e56ab356f9c37d809a104dc6bbbf4310b7676c
Ukran004 <ukrantandukar13@gmail.com>
2024-11-19 15:57:57 +0545

    classwok

---
+++ print_name_100times.dart
@@ -0,0 +1,10 @@
+void main(){
+  printName100Times("Your Name");
+}
+
+// 4. Print a name 100 times
+void printName100Times(String name) {
+  for (int i = 0; i < 100; i++) {
+    print(name);
+  }
+}
